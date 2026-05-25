0a7bd92d5fe5288d4f7293961107d424a85fd1bb
Tyler Wittig <44188028+twit96@users.noreply.github.com>
2026-05-25 10:45:47 -0500

    content: retire TCC 2026 as default intro

--- src/data/intros.js
+++ src/data/intros.js
@@ -1,27 +1,23 @@
-/*
-FIXME: defaultIntro is currently set as the TCC 2026 message because all cards
-currently in circulation were handed out at that conference without a ?intro=
-param. Before printing cards for a new audience:
-
-  1. Add a new entry to intros for the new event/context
-  2. Make defaultIntro a generic message
-  3. Remove tcc2026 from intros (no cards will ever hit it via URL param)
-
-TCC 2026 attendees who scan old cards will then get the generic message,
-which is an acceptable tradeoff.
-*/
 export const intros = {
-  tcc2026: {
-    subject: 'Following up after TCC 2026',
-    body: [
-      'Hi Tyler,',
-      '',
-      'It was nice meeting you at TCC 2026 in San Antonio.',
-      '',
-      'I look forward to keeping in touch.',
-    ].join('\r\n'),
-  },
-  // add future intros here, e.g. conf2027: { subject: '...', body: '...' }
+  // example27: {
+  //   subject: 'Following up after Example Event 2027',
+  //   body: [
+  //     'Hi Tyler,',
+  //     '',
+  //     'It was nice meeting you at Example Event 2027.',
+  //     '',
+  //     'I look forward to keeping in touch.',
+  //   ].join('\r\n'),
+  // },
 };
 
-export const defaultIntro = intros.tcc2026;
+export const defaultIntro = {
+  subject: 'Following up on your card',
+  body: [
+    'Hi Tyler,',
+    '',
+    'I got your business card and wanted to reach out.',
+    '',
+    'Hope to stay in touch.',
+  ].join('\r\n'),
+};
