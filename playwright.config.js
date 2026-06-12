21e5c0bb5a29400000a541da1c75983b73a58be4
Tyler Wittig <44188028+twit96@users.noreply.github.com>
2026-06-11 20:59:36 -0500

    chore: update playwright mobile device descriptors

--- playwright.config.js
+++ playwright.config.js
@@ -15,8 +15,8 @@ export default defineConfig({
     { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
     { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
     { name: 'webkit', use: { ...devices['Desktop Safari'] } },
-    { name: 'Mobile Chrome', use: { ...devices['Pixel 5'] } },
-    { name: 'Mobile Safari', use: { ...devices['iPhone 12'] } },
+    { name: 'Mobile Chrome', use: { ...devices['Pixel 7'] } },
+    { name: 'Mobile Safari', use: { ...devices['iPhone 15 Pro'] } },
   ],
   webServer: {
     command: 'npm run dev',
